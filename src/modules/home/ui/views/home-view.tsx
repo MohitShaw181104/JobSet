b049b17385b22d3f5ed7ce30610ce223bfae41a8
mohitfrbatman <mohitshaw181104@gmail.com>
2026-01-10 11:04:27 +0530

    6: Dashboard-Sidebar

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -8,23 +8,23 @@ import { useRouter } from 'next/navigation';
 
 export const HomeView = () => {
 
-  const {data: session} = authClient.useSession();
-  const Router = useRouter();
+    const {data: session} = authClient.useSession();
+    const Router = useRouter();
 
-  if(!session){
+    if(!session){
+        return (
+        <p>Loading...</p>
+        )
+    }
+
     return (
-      <p>Loading...</p>
+        <div className="flex flex-col p-4 gap-y-4">
+            <h1>Welcome {session.user.name}</h1>
+            <Button onClick={() => authClient.signOut({fetchOptions: {onSuccess: () => Router.push('/sign-in')}})}>
+            Sign Out
+            </Button>
+        </div>
     )
-  }
-
-  return (
-    <div className="flex flex-col p-4 gap-y-4">
-        <h1>Welcome {session.user.name}</h1>
-        <Button onClick={() => authClient.signOut({fetchOptions: {onSuccess: () => Router.push('/sign-in')}})}>
-          Sign Out
-        </Button>
-      </div>
-  )
 }
 
 export default HomeView
