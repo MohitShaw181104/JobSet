b20b2650acd006e5a6e1b2a2753dbd494068548e
mohitfrbatman <mohitshaw181104@gmail.com>
2026-01-12 21:58:44 +0530

    18: Meetings Data Table

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { AgentGetOne } from "../../types"
+import { AgentsGetMany } from "../../types"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 import { Badge} from "@/components/ui/badge"
 import { VideoIcon, CornerDownRightIcon } from "lucide-react"
 
     // This type is used to define the shape of our data.
     // You can use a Zod schema here if you want.
-export const columns: ColumnDef<AgentGetOne>[] = [
+export const columns: ColumnDef<AgentsGetMany[number]>[] = [
     {
         accessorKey: "name",
         header: "Agent Name",
