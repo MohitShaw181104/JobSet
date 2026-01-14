d7527ff102370e8c005453f5932cd21aa3422070
mohitfrbatman <mohitshaw181104@gmail.com>
2026-01-14 22:22:34 +0530

    26: Transcript and Chat

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,48 +1,55 @@
-import { AgentsListHeader } from '@/modules/agents/ui/components/agents-list-header';
-import { AgentsView, AgentsViewError, AgentsViewLoading } from '@/modules/agents/ui/views/agents-view'
-import { getQueryClient, trpc } from '@/trpc/server'
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import React, { Suspense } from 'react'
-import { ErrorBoundary } from "react-error-boundary";
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { SearchParams } from 'nuqs/server';
-import { loadSearchparams } from '@/modules/agents/params';
+    import { AgentsListHeader } from '@/modules/agents/ui/components/agents-list-header';
+    import {
+    AgentsView,
+    AgentsViewError,
+    AgentsViewLoading,
+    } from '@/modules/agents/ui/views/agents-view';
+    import { getQueryClient, trpc } from '@/trpc/server';
+    import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+    import React, { Suspense } from 'react';
+    import { ErrorBoundary } from 'react-error-boundary';
+    import { auth } from '@/lib/auth';
+    import { headers } from 'next/headers';
+    import { redirect } from 'next/navigation';
+    import { SearchParams } from 'nuqs/server';
+    import { loadSearchParams } from '@/modules/agents/params';
 
-interface Props{
-    searchParams: Promise<SearchParams>
-};
+    interface Props {
+    searchParams: SearchParams;
+    }
 
+    const Page = async ({ searchParams }: Props) => {
+    const filters = await loadSearchParams(searchParams);
 
-const Page = async ({searchParams}: Props) => {
-
-    const filters= await loadSearchparams(searchParams);
-
-    const session= await auth.api.getSession({
+    const session = await auth.api.getSession({
         headers: await headers(),
-        });
-        
-        if(!session){
-            redirect("/sign-in");
-        }
+    });
+
+    if (!session) {
+        redirect('/sign-in');
+    }
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
-        ...filters,
-    }));
 
-    return ( 
+    void queryClient.prefetchQuery(
+        trpc.agents.getMany.queryOptions({
+        ...filters,
+        })
+    );
+
+    return (
         <>
-        <AgentsListHeader/>
+        <AgentsListHeader />
+
         <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<AgentsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewError />}>
-        <AgentsView />
-        </ErrorBoundary>
-        </Suspense>
+            <Suspense fallback={<AgentsViewLoading />}>
+            <ErrorBoundary fallback={<AgentsViewError />}>
+                <AgentsView />
+            </ErrorBoundary>
+            </Suspense>
         </HydrationBoundary>
         </>
-    )}
+    );
+    };
 
-export default Page
+    export default Page;
